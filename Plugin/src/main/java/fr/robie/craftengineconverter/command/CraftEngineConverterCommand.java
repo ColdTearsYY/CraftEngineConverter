6a9b3720fd151860eef345714afdab180a86ad2e
1robie <clarence@aheritier.net>
2026-01-23 17:56:46 +0100

    fix: build

--- Plugin/src/main/java/fr/robie/craftengineconverter/command/CraftEngineConverterCommand.java
+++ Plugin/src/main/java/fr/robie/craftengineconverter/command/CraftEngineConverterCommand.java
@@ -1,15 +1,9 @@
 package fr.robie.craftengineconverter.command;
 
-import com.moulberry.axiom.paperapi.AxiomCustomBlocksAPI;
-import com.moulberry.axiom.paperapi.block.AxiomCustomBlockBuilder;
 import fr.robie.craftengineconverter.CraftEngineConverter;
 import fr.robie.craftengineconverter.common.permission.Permission;
 import fr.robie.craftengineconverter.utils.command.CommandType;
 import fr.robie.craftengineconverter.utils.command.VCommand;
-import net.kyori.adventure.key.Key;
-import org.bukkit.Material;
-import org.bukkit.block.data.BlockData;
-import org.bukkit.inventory.ItemStack;
 
 public class CraftEngineConverterCommand extends VCommand {
     public CraftEngineConverterCommand(CraftEngineConverter craftEngineConverter) {
@@ -23,16 +17,6 @@ public class CraftEngineConverterCommand extends VCommand {
     @Override
     protected CommandType perform(CraftEngineConverter plugin) {
         syntaxMessage();
-        AxiomCustomBlocksAPI api = AxiomCustomBlocksAPI.getAPI();
-        BlockData blockData = plugin.getServer().createBlockData(Material.DIAMOND_BLOCK);
-        AxiomCustomBlockBuilder helloWorld = api.createSingle(Key.key("test:test"), "hello world", blockData);
-        helloWorld.sendServerPickBlockIfPossible(true);
-        helloWorld.pickBlockItemStack(new ItemStack(Material.DIAMOND_BLOCK));
-        try {
-            api.register(plugin, helloWorld);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
         return CommandType.SUCCESS;
     }
 }
